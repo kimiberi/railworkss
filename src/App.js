6c2c24a1bc8d1af0b46368b9a62d0954168f095d
Kimberly <you@example.com>
2020-10-05 23:54:28 +0800

    create test.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React from 'react'
 import Header from './components/pages/Header/Header'
 import Homepage from './components/pages/Homepage/Homepage'
 import Footer from './components/pages/Footer/Footer'
+import Test from './components/pages/Test/Test'
+import 'semantic-ui-css/semantic.min.css'
 
 function App() {
   return (
     <div>
+      {/* <Test /> */}
       <Header />
       <Homepage />
       <Footer />
---
+++ src/components/pages/Test/Test.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import './Test.scss'
+
+export class Test extends Component {
+  render() {
+    return <div className='test-style'></div>
+  }
+}
+
+export default Test
